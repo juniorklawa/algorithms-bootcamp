45f39e686a038bdd06ec357d43f2f5cbb2435e50
everaldo@c3po <juniorklawa10@gmail.com>
2020-09-08 14:09:34 -0300

    wip

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,7 +9,38 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    
+  const formattedA = stringA.split(" ").join("").toLowerCase();
+
+  const formattedB = stringB.split(" ").join("").toLowerCase();
+
+  const mapA = {};
+  const mapB = {};
+  let flag = true;
+
+  for (let char of formattedA) {
+    if (!mapA[char]) {
+      mapA[char] = 1;
+    } else {
+      mapA[char]++;
+    }
+  }
+
+  for (let char of formattedB) {
+    if (!mapB[char]) {
+      mapB[char] = 1;
+    } else {
+      mapB[char]++;
+    }
+  }
+
+  for (let propA in mapA) {
+    if (!(mapB[propA] === mapA[propA])) {
+      flag = false;
+      break;
+    }
+  }
+
+  return flag;
 }
 
 module.exports = anagrams;
