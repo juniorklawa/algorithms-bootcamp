6c4feee0d351b0fe59dfa0bcab96ef2e4a4e9976
everaldo@bb-8 <juniorklawa10@gmail.com>
2020-09-10 11:16:12 -0300

    finished steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,54 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  let a = "";
+// Solution 1
+// function steps(n) {
+//   let a = "";
 
-  for (let i = 0; i < n; i++) {
-    a += " ";
+//   for (let i = 0; i < n; i++) {
+//     a += " ";
+//   }
+
+//   const formattedA = a.split("");
+
+//   for (let i = 0; i < n; i++) {
+//     formattedA[i] = "#";
+//     console.log(formattedA.join(""));
+//   }
+// }
+
+// Solution 2
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let stair = ''
+//     for (let j = 0; j< n; j++){
+//       if(j<=i){
+//         stair+= '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair)
+//   }
+// }
+
+function step(n, row = 0, stair = '') {
+    if (n === row) {
+      return;
+    }
+  
+    if (n === stair.length) {
+      console.log(stair)
+      return step(n, row + 1);
+    }
+  
+    if (stair.length <= row) {
+      stair += '#';
+    } else {
+      stair += ' ';
+    }
+    step(n, row, stair)
   }
-
-  const formattedA = a.split("");
-
-  for (let i = 0; i < n; i++) {
-    formattedA[i] = "#";
-    console.log(formattedA.join(""));
-  }
-}
 
 module.exports = steps;
