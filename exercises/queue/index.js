ef2f32229a55df2b3c14bda17e10368e9474d5bf
everaldo@bb-8 <juniorklawa10@gmail.com>
2020-09-16 20:01:04 -0300

    queues

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,19 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(selectedNumber) {
+    this.queue.push(selectedNumber);
+  }
+
+  remove() {
+    const removed = this.queue.shift();
+    return removed;
+  }
+}
 
 module.exports = Queue;
