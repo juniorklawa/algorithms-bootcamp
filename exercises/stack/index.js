56ecae986e0cd0a7a70d4b1709abeaf76bfeaad1
everaldo@bb-8 <juniorklawa10@gmail.com>
2020-09-19 18:36:10 -0300

    stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(numero) {
+    this.stack.push(numero);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+}
 
 module.exports = Stack;
